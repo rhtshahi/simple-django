1802dacd55a49a52347be66933e77210730c5b17
rhtshahi <iamshahirohit@gmail.com>
2025-06-11 13:14:23 +0545

    updated: view

--- app1/views.py
+++ app1/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(requests):
-    return HttpResponse("This is Homepage!!!")
+    return HttpResponse("Hello World!!!")
